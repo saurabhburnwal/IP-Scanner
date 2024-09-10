3eb9470776a11b2ad6b48e744ef34be073a3d66c
Saurabh Burnwal <saurabhburnwal00@gmail.com>
2024-09-10 21:46:23 +0530

    define the portscan function using TCP socket

--- ip_scanner.py
+++ ip_scanner.py
@@ -9,3 +9,14 @@ print_lock = threading.Lock()
 target = input("Enter the IP address to scan: ")
 t_IP = socket.gethostbyname(target)
 print("Starting scan on host: ", t_IP)
+
+def portscan(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        con = s.connect((t_IP, port))
+        with print_lock:
+            print(port, "is open")
+        con.close()
+    except:
+        pass
+
