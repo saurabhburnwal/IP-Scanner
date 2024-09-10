01ef90df2a6249e2037e18a1698d941e13f02905
Saurabh Burnwal <saurabhburnwal00@gmail.com>
2024-09-10 21:42:05 +0530

    initialize thread objects and input variables

--- ip_scanner.py
+++ ip_scanner.py
@@ -2,3 +2,10 @@ import socket
 import time
 import threading
 from queue import Queue
+
+socket.setdefaulttimeout(0.25)
+print_lock = threading.Lock()
+
+target = input("Enter the IP address to scan: ")
+t_IP = socket.gethostbyname(target)
+print("Starting scan on host: ", t_IP)
